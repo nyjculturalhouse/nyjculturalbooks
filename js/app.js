013fe4752cdfd9e73aaf8064d07be4aabc2968fc
nyjculturalhouse <nyjculturalhouse@gmail.com>
2026-05-13 14:02:05 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // 본인의 GAS 웹앱 URL로 반드시 변경하세요.
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycbxZbmTULBjxlBMGTd2ss1V5hcqoyFroGoZ1Cux6oM4tcDYMKcXsiMnnhG5l5Zr3a3En/exec';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbyYNotHbzXFkJlo3371co_DDRLYshI1uBOrou8ZgSY19a5q2g3AfNikdAaozqYr8ySL/exec';
 
 // 로그인 상태 체크
 function checkAuth(requireAdmin = false) {
