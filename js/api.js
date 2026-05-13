0bfff923f917081c314ca1cfa40bafd42f85c511
nyjculturalhouse <nyjculturalhouse@gmail.com>
2026-05-13 15:03:24 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,13 +1,13 @@
 // js/api.js 맨 위
 
 // 다른 파일에서 window.GAS_URL을 못 가져오는 문제를 방지하기 위해 직접 선언
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycbyHmROHxGfgjPXqWz5n8WAFqzPcxESJnYzd-YLXa-bTpZm5boVMerAvDnQ89Fqj-jHu/exec';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycby8hvXjgaLn8SqlKtM2-PrROvd3q2D5rKqYfPVZTngCfIV2Ep6hGsVidyw6MReGMU_b/exec';
 
 const API = {
     async post(action, payload = {}) {
         UI.showLoading();
         // 주소가 undefined인지 여기서 최종 확인
-        const targetUrl = window.GAS_URL || 'https://script.google.com/macros/s/AKfycbyHmROHxGfgjPXqWz5n8WAFqzPcxESJnYzd-YLXa-bTpZm5boVMerAvDnQ89Fqj-jHu/exec';
+        const targetUrl = window.GAS_URL || 'https://script.google.com/macros/s/AKfycby8hvXjgaLn8SqlKtM2-PrROvd3q2D5rKqYfPVZTngCfIV2Ep6hGsVidyw6MReGMU_b/exec';
         
         try {
             const response = await fetch(targetUrl, {
